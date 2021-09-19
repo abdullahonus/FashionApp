9b7f58e3291425c891ef0e8b7e9eb8f6d8d0288d
abdullahonus <abdullah.onus0@gmail.com>
2021-09-19 11:23:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Countrol4Offical",
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
